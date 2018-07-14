41287cc3759f7089b27722a51c9e3dc0c32c643c
Dan Klinga <dklinga@Dans-MacBook-Pro.local>
2018-07-14 13:19:16 -0400

    card fix

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -3,46 +3,46 @@ import "./Card.css";
 
 const Card = props => (
     <div>
-    <div class="card">
-    <div class="card-header bg-info"></div>
-    <div class="card-body">
-      <div class="header-div">
-        <div class="user-photo-div">
-          <img src="./../img/cw-round2.png" class="user-photo" />
+    <div className="card">
+    <div className="card-header bg-info"></div>
+    <div className="card-body">
+      <div className="header-div">
+        <div className="user-photo-div">
+          <img src="./../img/cw-round2.png" class="user-photo" alt="Card"/>
         </div>
-        <div class="header-info">
+        <div className="header-info">
           <h3>Dan Klinga</h3>
           <h5>dklinga@text.com</h5>
           <p>555 5785</p>
         </div>
       </div>
-      <div class="social-div">
-        <div class="social-inner twitter">
-          <div class="contact-image mr-2">
-            <i class="fab fa-twitter"></i>
+      <div className="social-div">
+        <div className="social-inner twitter">
+          <div className="contact-image mr-2">
+            <i className="fab fa-twitter"></i>
           </div>
-          <a href="http://www.twitter.com/@twitter" target="_blank" class="links">@@twitter</a>
+          <a href="http://www.twitter.com/@twitter" target="_blank" rel="noopener noreferrer" className="links">@twitter</a>
         </div>
-        <div class="social-inner fb">
-          <div class="contact-image mr-2">
-            <i class="fab fa-facebook"></i>
+        <div className="social-inner fb">
+          <div className="contact-image mr-2">
+            <i className="fab fa-facebook"></i>
           </div>
-          <a href="http://www.facebook.com/@facebook" target="_blank" class="links">@facebook</a>
+          <a href="http://www.facebook.com/@facebook" target="_blank" rel="noopener noreferrer" className="links">@facebook</a>
         </div>
-        <div class="social-inner linkedin">
-          <div class="contact-image mr-2">
-            <i class="fab fa-linkedin"></i>
+        <div className="social-inner linkedin">
+          <div className="contact-image mr-2">
+            <i className="fab fa-linkedin"></i>
           </div>
-          <a href="http://www.linkedin.com/in/@linked in" target="_blank" class="links">@linked in</a>
+          <a href="http://www.linkedin.com/in/@linked in" target="_blank" rel="noopener noreferrer" className="links">@linked in</a>
         </div>
-        <div class="social-inner github">
-          <div class="contact-image mr-2">
-            <i class="fab fa-github"></i>
+        <div className="social-inner github">
+          <div className="contact-image mr-2">
+            <i className="fab fa-github"></i>
           </div>
-          <a href="http://www.github.com/@github" target="_blank" class="links">@github</a>
+          <a href="http://www.github.com/@github" target="_blank" rel="noopener noreferrer" className="links">@github</a>
         </div>
-        <button class="btn btn-danger export-button">Export
-          <i class="fas fa-download"></i>
+        <button className="btn btn-danger export-button">Export
+          <i className="fas fa-download"></i>
         </button>
       </div>
     </div>
